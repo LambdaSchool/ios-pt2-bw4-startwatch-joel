cf84ba765ab317053e40df9912ab57d48ce30647
Joel Groomer <joelgroomer@gmail.com>
2020-02-25 23:08:03 -0800

    Created TaskColor enum and added extension to UIColor to utilize it

---
+++ StartWatch/Helpers/UIColor+taskColor.swift
@@ -0,0 +1,60 @@
+//
+//  UIColor+taskColor.swift
+//  StartWatch
+//
+//  Created by Joel Groomer on 2/25/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum TaskColor: Int {
+    case black = 0
+    case red = 1
+    case orange = 2
+    case yellow = 3
+    case green = 4
+    case blue = 5
+    case indigo = 6
+    case lavender = 7
+    case pink = 8
+    case white = 9
+    case gray = 10
+    case brown = 11
+}
+
+extension UIColor {
+    func taskColor(_ taskColor: TaskColor) -> UIColor {
+        let color: UIColor
+        
+        switch taskColor {
+        case .black:
+            color = UIColor.black
+        case .red:
+            color = UIColor.systemRed
+        case .orange:
+            color = UIColor.systemOrange
+        case .yellow:
+            color = UIColor.systemYellow
+        case .green:
+            color = UIColor.systemGreen
+        case .blue:
+            color = UIColor.systemBlue
+        case .indigo:
+            color = UIColor.systemIndigo
+        case .lavender:
+            color = UIColor.systemPurple
+        case .pink:
+            color = UIColor.systemPink
+        case .white:
+            color = UIColor.white
+        case .gray:
+            color = UIColor.systemGray
+        case .brown:
+            color = UIColor.brown
+        }
+        
+        return color
+    }
+}
