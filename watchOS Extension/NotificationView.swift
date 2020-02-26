ee43cec909b5bb9f06dd998fc0c81452103b41e1
Joel Groomer <joelgroomer@gmail.com>
2020-02-25 20:45:26 -0800

    Xcode project created
    Xcode project created and organized. Closes #1

---
+++ watchOS Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  watchOS Extension
+//
+//  Created by Joel Groomer on 2/25/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
