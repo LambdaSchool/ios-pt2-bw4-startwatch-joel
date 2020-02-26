df2752f01442607884209f24386132c396f893c0
Joel Groomer <joelgroomer@gmail.com>
2020-02-25 20:45:26 -0800

    Xcode project created
    Xcode project created and organized. Closes #1

---
+++ watchOS Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watchOS Extension
+//
+//  Created by Joel Groomer on 2/25/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watchOS Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  watchOS Extension
+//
+//  Created by Joel Groomer on 2/25/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
