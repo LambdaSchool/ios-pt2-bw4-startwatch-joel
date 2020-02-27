6c126e6b0fe0538bf3a39f146c2c542955246550
Joel Groomer <joelgroomer@gmail.com>
2020-02-26 18:41:42 -0800

    added CoreDataStack.swift

---
+++ StartWatch/Models/CoreDataStack.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataStack.swift
+//  StartWatch
+//
+//  Created by Joel Groomer on 2/26/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+       let container = NSPersistentContainer(name: "StartWatch")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        let context = container.viewContext
+        context.automaticallyMergesChangesFromParent = true
+        return context
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+}
